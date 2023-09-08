fa8fef66f76eea951e561b0a508d4271b2ae7537
Yana Mishyna <yana.mishina.92@gmail.com>
2023-09-08 11:18:42 -0400

    created SVG file class and methods

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require('inquirer');
+const {writeFile} = require('fs/promises');
+const SVG = require('./lib/svg');
+const {Circle, Triangle, Square} = require('./lib/shapes');
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
